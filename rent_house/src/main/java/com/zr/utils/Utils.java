fe9e3e3c9e48e85b2b787505512f5bf4d033d04c
曹亮 <cl18271728774@163.com>
2021-10-13 16:41:30 +0800

    更新了Utils工具类，新增了 1. 对文件名进行（无重复)重名的方法 2. 文件上传方法

--- rent_house/src/main/java/com/zr/utils/Utils.java
+++ rent_house/src/main/java/com/zr/utils/Utils.java
@@ -1,9 +1,23 @@
 package com.zr.utils;
 
+import com.jspsmart.upload.Request;
+import com.jspsmart.upload.SmartUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.Random;
 
 public class Utils {
+    /**
+     * 消息摘要方法，md5加密
+     * @param msg
+     * @return
+     */
     public static String msgEncrypt(String msg){
         String result = "";
         try {
@@ -15,5 +29,40 @@ public class Utils {
             e.printStackTrace();
         }
         return result;
+    }
+
+    /**
+     * 产生不重复的文件名
+     * @param extension
+     * @return
+     */
+    public static String randomFileName(String extension){
+        Random random = new Random();
+        int randomNum = random.nextInt(90000)+10000;
+        return String.valueOf(new Date().getTime()) + randomNum+"."+extension;
+    }
+
+    /**
+     * 上传文件到指定的相对路径
+     * @param servlet
+     * @param request
+     * @param response
+     * @param path
+     * @return
+     */
+    public static boolean fileUpload(HttpServlet servlet, HttpServletRequest request, HttpServletResponse response,String path){
+        String realPath = servlet.getServletContext().getRealPath(path);
+        SmartUpload smartUpload = new SmartUpload();
+        try {
+            smartUpload.initialize(servlet.getServletConfig(),request,response);
+            smartUpload.upload();
+            Request uploadRequest = smartUpload.getRequest();
+            String extension = smartUpload.getFiles().getFile(0).getFileExt();
+            smartUpload.getFiles().getFile(0).saveAs(realPath+randomFileName(extension));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
