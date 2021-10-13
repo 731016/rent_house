8d19ee9f4e66f580fdd44a11a67620c52ca5bffd
zd <1936948982@qq.com>
2021-10-13 11:58:46 +0800

    loginSvl的实现

--- rent_house/src/main/java/com/zr/web/LoginSvl.java
+++ rent_house/src/main/java/com/zr/web/LoginSvl.java
@@ -1,5 +1,7 @@
 package com.zr.web;
 
+import com.alibaba.fastjson.JSON;
+import com.zr.pojo.ReturnResult;
 import com.zr.pojo.UserInfo;
 import com.zr.service.UserInfoService;
 import com.zr.utils.JDBCUtils;
@@ -10,7 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
+
 
 @WebServlet(name = "LoginSvl",urlPatterns = "/login")
 public class LoginSvl extends HttpServlet {
@@ -18,6 +23,18 @@ public class LoginSvl extends HttpServlet {
         String account = request.getParameter("account");
         String password = request.getParameter("password");
         UserInfoService user = UserInfoService.getInstance();
+        boolean login = user.login(account, password);
+        HttpSession session = request.getSession();
+        ReturnResult result = new ReturnResult(null,null,login);
+        String s = JSON.toJSONString(result);
+//        String s = "{\"log\":"+login+"}";
+        response.getWriter().print(s);
+        if (login){
+            UserInfo userInfo = user.getUserInfoByAccount(account);
+            session.setAttribute("user",userInfo);
+            response.getWriter().print("<script>history.go(-2)</script>");
+        }
+
 
     }
 
--- rent_house/src/main/webapp/login.jsp
+++ rent_house/src/main/webapp/login.jsp
@@ -9,6 +9,32 @@
   <link rel="stylesheet" href="./resource/css/login.css">
   <script src="resource/js/jquery-3.6.0.min.js" type="text/javascript" charset="utf-8"></script>
   <script src="resource/js/login.js"></script>
+  <script>
+    $(function () {
+      $("form").submit(function () {
+        let $txt = $("#txt").val();
+        let $psw = $("#psw").val();
+        let aj = '{"account":'+$txt+',"password":'+$psw+'}'
+        $.ajax({
+          type:"post",
+          data:aj,
+          dataType:"json",
+          url:"/login",
+          success:function (data) {
+            if (data.flag){
+              return true;
+            }else {
+              $("#validation").html("<span>账号或密码错误</span>").css("color","red")
+              return false;
+            }
+          },
+          error:function (e) {
+            $("body").html(e.responseText)
+          }
+        })
+      })
+    })
+  </script>
 </head>
 <body>
 <form method="post" action="">
@@ -19,6 +45,7 @@
       <div id="title" style="text-align: center;">
         <h1>登录</h1>
       </div>
+      <div id="validation"></div>
       <div class="input">
         账号：
         <input id="txt" name="account" /><br />
