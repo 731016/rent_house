961bb583d1d07afa5d769fdbc399231b28bab6fb
曹亮 <cl18271728774@163.com>
2021-10-14 09:39:20 +0800

    no commit message

--- src/main/webapp/resource/js/housedetails.js
+++ src/main/webapp/resource/js/housedetails.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function(){
 	let big=document.getElementById("big");
 	let small=document.getElementsByClassName("small");
-	for (var i = 0; i < small.length; i++) {
+	for (let i = 0; i < small.length; i++) {
 		small[i].onclick=function(){
 			big.src=this.src;
 		}
