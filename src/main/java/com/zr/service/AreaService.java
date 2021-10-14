b6f46eae785a848acbb048c02625fcaf5849bb26
zd <1936948982@qq.com>
2021-10-14 16:51:21 +0800

    添加工厂创建对象的静态方法

--- src/main/java/com/zr/service/AreaService.java
+++ src/main/java/com/zr/service/AreaService.java
@@ -1,10 +1,15 @@
 package com.zr.service;
 
 import com.zr.pojo.Area;
+import com.zr.service.impl.AreaServiceImpl;
+import com.zr.utils.FactoryUtils;
 
 import java.util.List;
 
 public interface AreaService {
+    static AreaService getInstance(){
+        return (AreaService)FactoryUtils.createInstance(AreaServiceImpl.class.getName());
+    }
     int addArea(Area area);
     List<Area> getAllArea();
     List<Area> getAreaLikeName(String name);
