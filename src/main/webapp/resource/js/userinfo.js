938b94d4cc92918dc8e9fcb035e6478e75c72ed3
dz <1286592866@qq.com>
2021-10-15 10:43:29 +0800

    2021/10/15:个人信息及房东信息界面完成

---
+++ src/main/webapp/resource/js/userinfo.js
@@ -0,0 +1,39 @@
+$(function () {
+    $('#userinfo').click(function () {
+        $('#userhouse').removeClass('user_menu_a');
+        $(this).addClass('user_menu_a');
+        $('#userinfo_body').show();
+        $('#userhouse_body').hide();
+    })
+    $('#userhouse').click(function () {
+        $('#userinfo').removeClass('user_menu_a');
+        $(this).addClass('user_menu_a');
+        $('#userinfo_body').hide();
+        $('#userhouse_body').show();
+    })
+
+    $('#change_password').click(function () {
+        $('#password_tr').toggle();
+    })
+
+    $('#house_record').click(function () {
+        $('#userinfo_content').hide();
+        $('#house_record_content').show();
+
+    })
+
+    $('#user_info').click(function () {
+        $('#userinfo_content').show();
+        $('#house_record_content').hide();
+    })
+
+    $('#landlord_info').click(function () {
+        $('#userhouse_content').show();
+        $('#house_content').hide();
+    })
+
+    $('#house_info').click(function () {
+        $('#userhouse_content').hide();
+        $('#house_content').show();
+    })
+})
