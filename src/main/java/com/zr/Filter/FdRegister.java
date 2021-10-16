eb639191ce57a53e4d702e87aba0a2d2dca6291d
Hzz <xiaofei00152@gmail.com>
2021-10-16 11:14:30 +0800

    房东注册页面，过滤器

---
+++ src/main/java/com/zr/Filter/FdRegister.java
@@ -0,0 +1,37 @@
+package com.zr.Filter;
+
+import com.zr.pojo.Landlord;
+import com.zr.pojo.UserInfo;
+import com.zr.service.LandlordService;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "FdRegister")
+public class FdRegister implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpReq = (HttpServletRequest) req;
+        HttpServletResponse httpResp = (HttpServletResponse) resp;
+        Object userInfo = httpReq.getSession().getAttribute("UserInfo");
+        LandlordService landlordService = LandlordService.getInstance();
+        if (userInfo == null) {
+            httpResp.sendRedirect("/login.jsp");
+            return;
+        } else if (landlordService.getLandlordByAccount(((UserInfo)userInfo).getAccount())!=null) {
+            httpResp.sendRedirect("/renting.jsp");
+            return;
+        }
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
